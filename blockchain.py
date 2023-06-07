7a5472b2ed3231d46c09ec7cb24c1524ecb12fce
ibnaleem <ibnaleem@outlook.com>
2023-06-07 19:57:07 +0100

    create Block dataclass

--- blockchain.py
+++ blockchain.py
@@ -1,2 +1,12 @@
 from dataclasses import dataclass
 from time import time
+
+class Blockchain:
+    
+    @dataclass
+    class Block:
+        index: str
+        timestamp: float
+        transactions: list
+        proof: int
+        previousHash: int
