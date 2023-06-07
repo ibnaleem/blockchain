f14ab2f1cb6e9c71b0887d9c4ea6c93992c1933c
ibnaleem <ibnaleem@outlook.com>
2023-06-07 19:59:02 +0100

    create GenesisBlock dataclass; initialize Blockchain's attributes

--- blockchain.py
+++ blockchain.py
@@ -10,3 +10,16 @@ class Blockchain:
         transactions: list
         proof: int
         previousHash: int
+
+    class GenesisBlock(Block):
+        def __post_init__(self) -> None:
+            self.index = 0
+
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+
+    def __len__(self) -> int:
+        return len(self.chain)
+    
+    
