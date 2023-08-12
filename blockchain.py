7d96e4311e3d47eb0d50029cec7be1ccb61eb945
ibnaleem <ibnaleem@outlook.com>
2023-08-12 15:01:04 +0100

    /nodes/register endpoint

--- blockchain.py
+++ blockchain.py
@@ -197,3 +197,20 @@ def full_chain():
         'length': len(blockchain),
     }
     return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
